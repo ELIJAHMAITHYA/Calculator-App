c95c1c454f38497fc6e6734824a194feab31d315
Sila Elijah <elimaithya21@gmail.com>
2023-09-26 20:35:02 +0300

    First App Row and Answer

--- app/src/main/java/com/example/calculatorapp/Calculator.kt
+++ app/src/main/java/com/example/calculatorapp/Calculator.kt
@@ -1,9 +1,12 @@
 package com.example.calculatorapp
 
 import android.graphics.Paint.Align
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
@@ -11,6 +14,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.LightGray
+import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
@@ -20,6 +25,7 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun Calculator(
     state: CalculatorState,
+    onAction: (CalculatorActions) -> Unit,
     buttonSpacing: Dp = 8.dp,
     modifier: Modifier = Modifier
 ) {
@@ -46,6 +52,41 @@ fun Calculator(
                 fontSize = 80.sp
 
             )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(buttonSpacing)
+            ) {
+                CalculatorButton(
+                    symbol = "AC",
+                    modifier = Modifier
+                        .background(LightGray)
+                        .aspectRatio(2f)
+                        .weight(2f),
+                    onClick = {
+                        onAction(CalculatorActions.Clear)
+                    }
+                )
+                CalculatorButton(
+                    symbol = "Del",
+                    modifier = Modifier
+                        .background(LightGray)
+                        .aspectRatio(1f)
+                        .weight(1f),
+                    onClick = {
+                        onAction(CalculatorActions.Delete)
+                    }
+                )
+                CalculatorButton(
+                    symbol = "/",
+                    modifier = Modifier
+                        .background(Yellow)
+                        .aspectRatio(1f)
+                        .weight(1f),
+                    onClick = {
+                        onAction(CalculatorActions.Operation(CalculatorOperation.Divide))
+                    }
+                )
+            }
         }
     }
 
